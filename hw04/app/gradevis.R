d635e41537b1f84ec0243a8dd30b9e66a186dadc
Vibha Seshadri <vibhaseshadri@berkeley.edu>
2017-11-16 12:12:59 -0800

    finished shiny app

--- hw04/app/gradevis.R
+++ hw04/app/gradevis.R
@@ -15,6 +15,8 @@
 library(shiny)
 library(readr)
 library(ggplot2)
+library(ggvis)
+library(dplyr)
 
 # Data set cleanscores which will be used in this app
 cleanscores <- read_csv('../data/cleandata/cleanscores.csv')
@@ -29,9 +31,9 @@ for (g in grd) {
   tbl_ordered <- c(tbl_ordered, as.integer(tbl[g]))
   prop_ordered <- c(prop_ordered, round(as.numeric(prop_tbl[g]), 2))
 }
-grades <- data.frame("Grade" = grd,
-                     "Freq" = tbl_ordered,
-                     "Prop" = prop_ordered,
+grades <- data.frame(Grade = grd,
+                     Freq = tbl_ordered,
+                     Prop = prop_ordered,
                      stringsAsFactors = FALSE)
 
 # Define UI for application that draws a histogram
@@ -49,19 +51,32 @@ ui <- fluidPage(
                          ),
         conditionalPanel(condition = "input.tabselected==2",
                          selectInput("histx", label =  h3("X-Axis Variable"),
-                                     choices = attributes(cleanscores)$names),
-                         sliderInput("histbin", label = h3("Bin Width"), min = 0, 
-                                     max = 10, value = 10))
+                                     choices = attributes(cleanscores)$names[1:22]),
+                         sliderInput("histbin", label = h3("Bin Width"), min = 1, 
+                                     max = 10, value = 10)),
+        conditionalPanel(condition = "input.tabselected==3",
+                         selectInput("scatx", label =  h3("X-Axis Variable"),
+                                     choices = attributes(cleanscores)$names[1:22]),
+                         selectInput("scaty", label =  h3("Y-Axis Variable"),
+                                     choices = attributes(cleanscores)$names[1:22]),
+                         sliderInput("scatOpacity", label = h3("Opacity"), min = 0, 
+                                     max = 1, value = 1)),
+                         radioButtons("radio", label = h3("Show Line"),
+                                       choices = list("None" = 0, "lm" = 1, "loess" = 2), 
+                                       selected = 0)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
         tabsetPanel(type = "tabs",
                     tabPanel("Barchart", value = 1,
-                             plotOutput("vis_barchart")),
+                             ggvisOutput("barchart")),
                     tabPanel("Histogram", value = 2,
-                             plotOutput("vis_histogram")),
-                    tabPanel("Scatterplot", value = 3),
+                             ggvisOutput("hist"),
+                             h2("Summary Statistics"),
+                             verbatimTextOutput("summary")),
+                    tabPanel("Scatterplot", value = 3,
+                             ggvisOutput("scatterplot")),
                     id = "tabselected")
       )
    )
@@ -75,17 +90,80 @@ server <- function(input, output) {
   })
   
   # Creating the barchart that goes with tab1
-  output$vis_barchart <- renderPlot({
-    ggplot() + 
-      geom_bar(data = grades, aes(x = Grade, y = Freq), stat = "identity") +
-      scale_x_discrete(limits = grd)
+  vis_barchart <- reactive({
+    grade <- prop("x", as.symbol("Grade"))
+    frequency <- prop("y", as.symbol("Freq"))
+    grades %>%
+      ggvis(x = grade, y = frequency, fill := "#439aca") %>% 
+      scale_ordinal('x', domain = grd) %>%
+      layer_bars(stroke := '#439aca', width = input$width,
+                 fillOpacity := 0.8, fillOpacity.hover := 1) %>%
+      add_axis("y", title = "frequency")
   })
   
+  vis_barchart %>% bind_shiny("barchart")
+  
   # Creating histogram that goes with tab2
-  output$vis_histogram <- renderPlot({
-    ggplot(data = cleanscores, aes_string(x = input$histx)) + 
-      geom_histogram(binwidth = 10, stat = "count")
+  vis_histogram <- reactive({
+    category <- prop("x", as.symbol(input$histx))
+    cleanscores %>% 
+      ggvis(x = category, fill := "#439aca") %>% 
+      layer_histograms(stroke := '#439aca', width = input$histbin,
+                       fillOpacity := 0.8, fillOpacity.hover := 1)
   })
+  
+  vis_histogram %>% bind_shiny("hist")
+  
+  # Creating Summary Statistics for chosen x axis
+  output$summary <- renderPrint({
+    switch(input$histx,
+           "HW1" = print_stats(summary_stats(cleanscores$HW1)),
+           "HW2" = print_stats(summary_stats(cleanscores$HW2)),
+           "HW3" = print_stats(summary_stats(cleanscores$HW3)),
+           "HW4" = print_stats(summary_stats(cleanscores$HW4)),
+           "HW5" = print_stats(summary_stats(cleanscores$HW5)),
+           "HW6" = print_stats(summary_stats(cleanscores$HW6)),
+           "HW7" = print_stats(summary_stats(cleanscores$HW7)),
+           "HW8" = print_stats(summary_stats(cleanscores$HW8)),
+           "HW9" = print_stats(summary_stats(cleanscores$HW9)),
+           "ATT" = print_stats(summary_stats(cleanscores$ATT)),
+           "QZ1" = print_stats(summary_stats(cleanscores$QZ1)),
+           "QZ2" = print_stats(summary_stats(cleanscores$QZ2)),
+           "QZ3" = print_stats(summary_stats(cleanscores$QZ3)),
+           "QZ4" = print_stats(summary_stats(cleanscores$QZ4)),
+           "EX1" = print_stats(summary_stats(cleanscores$EX1)),
+           "EX2" = print_stats(summary_stats(cleanscores$EX2)),
+           "Test1" = print_stats(summary_stats(cleanscores$Test1)),
+           "Test2" = print_stats(summary_stats(cleanscores$Test2)),
+           "Homework" = print_stats(summary_stats(cleanscores$Homework)),
+           "Quiz" = print_stats(summary_stats(cleanscores$Quiz)),
+           "Lab" = print_stats(summary_stats(cleanscores$Lab)),
+           "Overall" = print_stats(summary_stats(cleanscores$Overall))
+    )
+  })
+  
+  # Creating scatterplot that goes with tab3
+  vis_scatterplot <- reactive({
+    xvar <- prop("x", as.symbol(input$scatx))
+    yvar <- prop("y", as.symbol(input$scaty))
+    if (input$radio == 0) {
+      cleanscores %>% 
+        ggvis(x = xvar, y = yvar,  fill := "#439aca") %>% 
+        layer_points(stroke := '#439aca', fillOpacity := input$scatOpacity)
+    } else if (input$radio == 1) {
+      cleanscores %>% 
+        ggvis(x = xvar, y = yvar,  fill := "#439aca") %>% 
+        layer_points(stroke := '#439aca', fillOpacity := input$scatOpacity) %>%
+        layer_model_predictions(stroke := "black", model = "lm")
+    } else {
+      cleanscores %>% 
+        ggvis(x = xvar, y = yvar,  fill := "#439aca") %>% 
+        layer_points(stroke := '#439aca', fillOpacity := input$scatOpacity) %>%
+        layer_smooths()
+    }
+  })
+  
+  vis_scatterplot %>% bind_shiny("scatterplot")
   
   
 }
